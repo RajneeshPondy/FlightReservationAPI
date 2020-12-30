4e824b913a6f5cd300be0f92da90e91d661e954b
RajneeshPython <rajneeshkmr668@gmail.com>
2020-12-30 15:13:58 +0530

    Authorization using Authentication Token and Custom Token

--- flightServices/urls.py
+++ flightServices/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.authtoken import views
+
+from flightApp.authentication import CustomAuthToken
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('flightApp.urls'))
+    path('api/', include('flightApp.urls')),
+    # path('api-token-auth/', views.obtain_auth_token),
+    path('api-token-auth/', CustomAuthToken.as_view())
+
+
 ]
